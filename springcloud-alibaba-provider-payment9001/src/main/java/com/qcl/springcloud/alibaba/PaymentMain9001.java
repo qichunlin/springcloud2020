c1773d3884f1e9ea63204e9a1f470c401ad7e1df
qichunlin <737795279@qq.com>
2020-08-15 17:31:19 +0800

    [UPDATE] SpringCloudAlibaba-Nacos的服务提供者9001和9002已经整合完成

---
+++ springcloud-alibaba-provider-payment9001/src/main/java/com/qcl/springcloud/alibaba/PaymentMain9001.java
@@ -0,0 +1,20 @@
+package com.qcl.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chunlin.qi@hand-china.com
+ * @version 1.0
+ * @description
+ * @date 2020/8/15
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class, args);
+    }
+}
