d72ce42edff7e60ed5ea02d16431ab6941ff991b
qichunlin <737795279@qq.com>
2020-09-03 14:28:52 +0800

    [UPDATE] SpringCloud Alibaba Sentinel的服务提供者9003/9004

---
+++ springcloud-alibaba-provider-payment9004/src/main/java/com/qcl/springcloud/alibaba/PaymentMain9004.java
@@ -0,0 +1,20 @@
+package com.qcl.springcloud.alibaba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author chunlin.qi@hand-china.com
+ * @version 1.0
+ * @description
+ * @date 2020/9/3
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class PaymentMain9004 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9004.class, args);
+    }
+}
