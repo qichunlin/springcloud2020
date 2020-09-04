5cf26787d7aac49b809a93a39a5e98ebf59dc99f
qichunlin <737795279@qq.com>
2020-09-04 15:41:34 +0800

    [UPDATE] SpringCloud Alibaba Seata server软件&配置文件&业务数据库表

---
+++ z-seata/t_account.sql
@@ -0,0 +1,11 @@
+CREATE TABLE t_account (
+`id` BIGINT ( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY COMMENT 'id',
+`user_id` BIGINT ( 11 ) DEFAULT NULL COMMENT '用户id',
+`total` DECIMAL ( 10, 0 ) DEFAULT NULL COMMENT '总额度',
+`used` DECIMAL ( 10, 0 ) DEFAULT NULL COMMENT '已用余额',
+`residue` DECIMAL ( 10, 0 ) DEFAULT 0 COMMENT '剩余可用额度'
+) ENGINE = INNODB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8;
+
+INSERT INTO seata_account.t_account(`id`,`user_id`,`total`,`used`,`residue`)VALUES(1,1,'1000','0',1000);
+
+SELECT * FROM t_account;
---
+++ z-seata/t_order.sql
@@ -0,0 +1,12 @@
+CREATE TABLE t_storage (
+`id` BIGINT ( 11 ) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+`product_id` BIGINT ( 11 ) DEFAULT NULL COMMENT '产品id',
+`total` INT ( 11 ) DEFAULT NULL COMMENT '总库存',
+`used` INT ( 11 ) DEFAULT NULL COMMENT '已用库存',
+`residue` INT ( 11 ) DEFAULT NULL COMMENT '剩余库存'
+) ENGINE = INNODB AUTO_INCREMENT = 2 DEFAULT CHARSET = utf8;
+
+INSERT INTO t_storage(`id`,`product_id`,`total`,`used`,`residue`) VALUES('1','1','100','0','100');
+
+
+SELECT * FROM t_storage;
